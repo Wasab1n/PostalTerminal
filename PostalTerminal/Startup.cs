284c7a9197856829c0cfbd63669325515fde26ee
MantasN <manttassn@gmail.com>
2016-05-24 15:00:45 +0300

    Empty MVC template

---
+++ PostalTerminal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PostalTerminal.Startup))]
+namespace PostalTerminal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
